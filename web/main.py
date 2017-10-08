2621609950140577ba246b394f4b27fd9c51565e
amogkam <amogkamsetty@yahoo.com>
2017-10-07 17:20:52 -0700

    basic server

--- web/main.py
+++ web/main.py
@@ -1,17 +1,43 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+#from models import db
+
+db = SQLAlchemy()
 
 app = Flask(__name__)
+app.config['DEBUG'] = True
 
+POSTGRES = {
+    'user': 'datashark',
+    'pw': 'datashark',
+    'db': 'datasharkdb',
+    'host':  'datashark-database.cwnzqu4zi2kl.us-west-1.rds.amazonaws.com',
+    'port': '5432'
+}
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://%(user)s:\
+%(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
+
+db.init_app(app)
 
 @app.route("/")
 def main():
+    db.session.execute('create table test(name text)')
+    db.session.commit()
     return render_template('index.html')
 
 
 @app.route('/results', methods=['POST'])
 def results():
     categ = request.form['categ']
-    return render_template('left-sidebar.html', cat = categ)
+    # insert = Test(name=categ)
+    # db.session.add(insert)
+    # db.session.commit()
+    db.session.execute('insert into test values(:val)', {'val': categ})
+    db.session.commit()
+    result = db.session.execute('select * from test')
+    db.session.commit()
+    r = result.first()
+    return render_template('left-sidebar.html', cat = r)
 
 if __name__ == "__main__":
     app.run()
