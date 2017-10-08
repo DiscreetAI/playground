ca5c916e2b4afc50811a762f7489f2ddbf9559f0
Kiddyboots216 <kiddyboots216@berkeley.edu>
2017-10-08 04:20:58 -0700

    donezo

---
+++ app/src/main/java/data/shark/activities/tab/HomeActivity.java
@@ -0,0 +1,144 @@
+package data.shark.activities.tab;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.RequiresApi;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.fitbit.authentication.AuthenticationHandler;
+import com.fitbit.authentication.AuthenticationManager;
+import com.fitbit.authentication.AuthenticationResult;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import data.shark.R;
+
+public class HomeActivity extends AppCompatActivity implements AuthenticationHandler {
+
+    private Button auth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home2);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        TextView money = (TextView) findViewById(R.id.money);
+        Activity self = this;
+        auth = (Button) findViewById(R.id.authorize);
+        auth.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AuthenticationManager.login(self);
+            }
+        });
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        AuthenticationManager.onActivityResult(requestCode, resultCode, data, (AuthenticationHandler) this);
+    }
+
+    public void onAuthFinished(AuthenticationResult authenticationResult) {
+        if (authenticationResult.isSuccessful()) {
+            new HomeActivity.GetMethodDemo().execute("http://127.0.0.1:5000/insert");
+            auth.setText("Authorized!");
+            //WOOT WOOT!! It worked!
+        } else {
+            new HomeActivity.GetMethodDemo().execute("http://127.0.0.1:5000/insert");
+            auth.setText("Fitbit Authorized!");
+            //Uh oh... errors...
+        }
+    }
+    public void onLoginClick(View view) {
+        AuthenticationManager.login(this);
+    }
+    @RequiresApi(api = Build.VERSION_CODES.CUPCAKE)
+    private class GetMethodDemo extends AsyncTask<String , Void ,String> {
+        String server_response;
+
+        @Override
+        protected String doInBackground(String... strings) {
+
+            URL url;
+            HttpURLConnection urlConnection = null;
+
+            try {
+                url = new URL(strings[0]);
+                urlConnection = (HttpURLConnection) url.openConnection();
+
+                int responseCode = urlConnection.getResponseCode();
+
+                if(responseCode == HttpURLConnection.HTTP_OK){
+                    server_response = readStream(urlConnection.getInputStream());
+                    Log.v("CatalogClient", server_response);
+                }
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            Log.e("Response", "" + server_response);
+
+
+        }
+
+// Converting InputStream to String
+
+        private String readStream(InputStream in) {
+            BufferedReader reader = null;
+            StringBuffer response = new StringBuffer();
+            try {
+                reader = new BufferedReader(new InputStreamReader(in));
+                String line = "";
+                while ((line = reader.readLine()) != null) {
+                    response.append(line);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+            return response.toString();
+        }
+    }
+}
--- app/src/main/java/data/shark/activities/tab/HomeFragment.java
+++ app/src/main/java/data/shark/activities/tab/HomeFragment.java
@@ -75,7 +75,6 @@ public class HomeFragment extends Fragment implements View.OnClickListener, Auth
     @RequiresApi(api = Build.VERSION_CODES.CUPCAKE)
     public void onAuthFinished(AuthenticationResult authenticationResult) {
         if (authenticationResult.isSuccessful()) {
-            new GetMethodDemo().execute("http://127.0.0.1:5000/insert");
         } else {
             //Uh oh... errors...
         }
@@ -135,73 +134,7 @@ public class HomeFragment extends Fragment implements View.OnClickListener, Auth
 //                startActivity(activeIntent);
 //                break;
             default:
-                AuthenticationManager.login(this.getActivity());
                 break;
-        }
-    }
-
-    @RequiresApi(api = Build.VERSION_CODES.CUPCAKE)
-    private class GetMethodDemo extends AsyncTask<String , Void ,String> {
-        String server_response;
-
-        @Override
-        protected String doInBackground(String... strings) {
-
-            URL url;
-            HttpURLConnection urlConnection = null;
-
-            try {
-                url = new URL(strings[0]);
-                urlConnection = (HttpURLConnection) url.openConnection();
-
-                int responseCode = urlConnection.getResponseCode();
-
-                if(responseCode == HttpURLConnection.HTTP_OK){
-                    server_response = readStream(urlConnection.getInputStream());
-                    Log.v("CatalogClient", server_response);
-                }
-
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(String s) {
-            super.onPostExecute(s);
-
-            Log.e("Response", "" + server_response);
-
-
-        }
-
-// Converting InputStream to String
-
-        private String readStream(InputStream in) {
-            BufferedReader reader = null;
-            StringBuffer response = new StringBuffer();
-            try {
-                reader = new BufferedReader(new InputStreamReader(in));
-                String line = "";
-                while ((line = reader.readLine()) != null) {
-                    response.append(line);
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            } finally {
-                if (reader != null) {
-                    try {
-                        reader.close();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-            return response.toString();
         }
     }
 }
