da8ef550cfd58d57ecdba8bb2312ddd8c78666dd
amogkam <amogkamsetty@yahoo.com>
2017-10-07 18:27:07 -0700

    added get_col function

--- web/main.py
+++ web/main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+import pandas as pd
 #from models import db
 
 db = SQLAlchemy()
@@ -19,25 +20,23 @@ application.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://%(user)s:\
 
 db.init_application(application)
 
+
 @application.route("/")
 def main():
-    db.session.execute('create table test(name text)')
-    db.session.commit()
     return render_template('index.html')
 
 
 @application.route('/results', methods=['POST'])
 def results():
     categ = request.form['categ']
-    # insert = Test(name=categ)
-    # db.session.add(insert)
-    # db.session.commit()
-    db.session.execute('insert into test values(:val)', {'val': categ})
+    col_names = get_columns(categ)
+    return render_template('left-sidebar.html', cat = categ)
+
+def get_columns(categ):
+    cmd = 'select col, description from metadata where table_name==:val'
+    result = db.session.execute(text(cmd), val=categ)
     db.session.commit()
-    result = db.session.execute('select * from test')
-    db.session.commit()
-    r = result.first()
-    return render_template('left-sidebar.html', cat = r)
+    return result.fetchall()
 
 if __name__ == "__main__":
     application.run()
