5bf1c6325f31a0783423b2979f95f340fcc0fb0b
Georgy Marrero <georgymarrero@gmail.com>
2017-12-29 12:45:03 -0800

    Change DB to postgresql

--- auth/core/settings.py
+++ auth/core/settings.py
@@ -71,7 +71,7 @@ WSGI_APPLICATION = 'core.wsgi.application'
 if 'RDS_HOSTNAME' in os.environ:
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.mysql',
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
             'NAME': os.environ['RDS_DB_NAME'],
             'USER': os.environ['RDS_USERNAME'],
             'PASSWORD': os.environ['RDS_PASSWORD'],
