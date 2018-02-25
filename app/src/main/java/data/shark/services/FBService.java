4687b115c75255c3fe64057954a6a5130bef9bb2
kiddyboots216 <kiddyboots216@berkeley.edu>
2018-02-24 17:49:56 -0800

    cleaning up

--- app/src/main/java/data/shark/services/FBService.java
+++
@@ -1,8 +0,0 @@
-package data.shark.services;
-
-/**
- * Created by QuickCap on 6/20/2017.
- */
-
-public interface FBService {
-}
--- fitbitapi/src/main/java/data/shark/api/exceptions/TokenExpiredException.java
+++
@@ -1,7 +0,0 @@
-package data.shark.api.exceptions;
-
-/**
- * Created by jboggess on 9/19/16.
- */
-public class TokenExpiredException extends FitbitAPIException {
-}
